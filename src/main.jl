2bd0d17a2c28c7f95d27548a4cdeb8017646247d
vinicius-augustus <augustovinicius17@protonmail.com>
2021-05-28 02:34:41 -0300

    raytraced red sphere

--- src/main.jl
+++ src/main.jl
@@ -18,10 +18,19 @@ lowerleftcorner = origin - horizontal/2 - vertical/2 - Vec3(0.0, 0.0, focallengh
 
 println("Image size $imgwidth x $imgwidth")
 
-function raycolor(ray::Ray)
-    t = 0.5 * ray.direction[2] + 1.0
-    (1-t)RGB(1.0, 1.0, 1.0) + t*RGB(0.5, 0.7, 1.0)
+function backgroundcolor(dir)
+    t = 0.5 * dir[2] + 1.0
+    (1-t)RGB(1.0, 1.0, 1.0) + t*RGB(0.5, 0.7, 1.0)    
 end
+
+function raycolor(ray::Ray, sphere::Sphere)
+    if hit(sphere, ray)
+        return RGB(1.0, 0.0, 0.0)
+    end
+    backgroundcolor(ray.direction)
+end
+
+s1 = Sphere(Vec3(0.0, 0.0, -1.0), 0.5)
 
 for j = 1:imgheight
     for i = 1:imgwidth
@@ -30,7 +39,7 @@ for j = 1:imgheight
         dir = lowerleftcorner + u*horizontal + v*vertical - origin
         ray = Ray(origin, dir)
 
-        image[j, i] = raycolor(ray)
+        image[j, i] = raycolor(ray, s1)
     end
 end
 
